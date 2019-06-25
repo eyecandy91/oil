e427410376b98728a7ae5ecbe75786c161398c9c
Henry puttick <hputtick@me.com>
2019-06-25 21:05:33 +0700

    search filemarkup additions

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,29 +10,26 @@
 ?>
 
 <?php
-$file_name         	= get_field('file_name');
-$file_pdf          	= get_field('file_pdf');
-$file_desc          = get_field('file_description');
-$link				= $file_pdf['url'];
-$icon				= $file_pdf['icon'];
+$file_name = get_field('file_name');
+$file_pdf = get_field('file_pdf');
+$file_desc = get_field('file_description');
+$link = $file_pdf['url'];
+$icon = $file_pdf['icon'];
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-    <a href="<?php echo $link ?>" rel="bookmark" target="_blank">
-        <div class="column is-narrow product_block single__product image">
-            <div class="box">
-                <div class="po-no" style="background-image: url('<?php echo $icon ?>')"></div>
-                <div><?php echo $file_name ?></div>
-                <div><?php echo $$file_desc ?></div>
+<a href="<?php echo $link ?>" rel="single-po-number" target="_blank" class="column is-full-mobile is-one-third-tablet is-one-quarter-desktop">
+    <article id="post-<?php the_ID();?>" <?php post_class();?> class="">
+        <div class="box is-paddingless image">
+            <div class="po-no">
+                <div style="background-image: url('<?php echo $icon ?>')"></div>
             </div>
+            <div class="po-no__name title is-6 is-marginless"><?php echo $file_name ?></div>
+            <div class="po-no__desc title is-7"><?php echo $file_desc ?></div>
         </div>
-    </a>
-    <?php 
-		//echo "<pre>";
+        <?php
+        //echo "<pre>";
         //print_r($file_pdf);
         //echo "</pre>";
-		?>
-
-
-</article>
+        ?>
+    </article>
+</a>
