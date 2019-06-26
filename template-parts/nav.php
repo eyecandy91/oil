e3dac441bd94edcea9cbabedf65dc756d13de08e
Henry puttick <hputtick@me.com>
2019-06-26 09:13:22 +0700

    nav mobile and non mobile logout btn

--- template-parts/nav.php
+++ template-parts/nav.php
@@ -1,21 +1,26 @@
 <header>
-    <nav id="navbar" class="navbar is-spaced navbar has-shadow is-spaced is-uppercase"
-        itemscope="itemscope" itemtype="https://schema.org/SiteHeader">
+    <nav id="navbar" class="navbar is-spaced navbar has-shadow is-spaced is-uppercase" itemscope="itemscope"
+        itemtype="https://schema.org/SiteHeader">
         <div class="container">
-        <div class="navbar-brand">
-            <div class="navbar-item">
+            <div class="navbar-brand">
+                <div class="navbar-item">
                     <?php echo get_custom_logo(); ?>
-                
+                </div>
+                <a role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            <?php
+            if ( wp_is_mobile() ) {
+                if (is_user_logged_in()) {?>
+                    <a class="button is-link mobile-logout" href="<?php echo esc_url( wp_logout_url(home_url())); ?>">Logout</a>
+                <?php }
+            } 
+            ?>
             </div>
-            <a role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
-        </div>
-
-        <div id="navMenuDocumentation" class="navbar-menu">
-            <div class="navbar-end ">
+            <div id="navMenuDocumentation" class="navbar-menu">
+                <div class="navbar-end ">
                 <?php
                 wp_nav_menu(array(
                     'theme_location'  => 'menu-1',
@@ -23,11 +28,13 @@
                     'menu_class'      => 'navbar-nav has-text-weight-bold',
                     'list_item_class' => 'nav-item',
                 ));
-                if (is_user_logged_in()) {?>
-                <a class="button is-link" href="<?php echo esc_url( wp_logout_url(home_url())); ?>">Logout</a>
-                <?php }?>
+                if ( !wp_is_mobile() ) {
+                    if (is_user_logged_in()) {?>
+                        <a class="button is-link not-mobile-logout" href="<?php echo esc_url( wp_logout_url(home_url())); ?>">Logout</a>
+                    <?php }
+                } ?>
+                </div>
             </div>
-        </div>
         </div>
     </nav>
 </header>
