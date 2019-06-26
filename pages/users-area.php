7f608af009c62f251499d568f91c9c4152d2f278
Henry puttick <hputtick@me.com>
2019-06-26 15:47:30 +0700

    portal design updates

--- pages/users-area.php
+++ pages/users-area.php
@@ -6,6 +6,10 @@
  */
 
 if (is_user_logged_in()) {
+    $current_user   = wp_get_current_user();
+    $first          = $current_user->user_firstname;
+    $last           = $current_user->user_lastname; 
+    $user           = $current_user->user_login;
     get_header();
     global $current_user;
     // $file_name = get_field('file_name');
@@ -19,29 +23,51 @@ if (is_user_logged_in()) {
         'posts_per_page' => -1,
     )
     );
+    $total = $loop->found_posts;
     // echo "<pre>";
     // print_r($loop);
     // echo "</pre>";
     ?>
 
 <section class="section">
+    <?php if( current_user_can('administrator') ) {  ?>
+
+
+
+    <?php } else { ?>
     <div class="columns is-mobile is-multiline is-centered has-text-centered">
+        
+        <div class="content">    
+            <h2 class="title has-text-weight-light is-marginless">Welcome back<span class="has-text-weight-bold">
+            <?php
+            if ($first && $last) {
+                echo $first. '&nbsp;' .$last; 
+            } else if ($first) {
+                echo $first;
+            } else {
+                echo $user;
+            } ?></span>, here are your files</h2>
+            <h4 class="subtitle is-6 has-text-grey-light">You currently have <?php echo $total ?> certificates in your portal</h4>
+        </div>
         <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url('/'); ?>">
             <div>
-                <input value="" name="s" id="s" class="input" type="text" placeholder="Search your files">
+                <input value="" name="s" id="s" class="input" type="text"
+                    placeholder="Search <?php if ($first && $last) { echo $first. ' ' .$last.'\'s'; } else if ($first) {echo $first.'\'s'; } else { echo $user;} ?> files">
                 <input id="searchsubmit" value="Search" class="button is-link" type="submit">
             </div>
         </form>
-        <h2>Your files</h2>
     </div>
     <div class="columns is-mobile is-multiline is-centered has-text-centered">
         <?php while ($loop->have_posts()): $loop->the_post();
         get_template_part('template-parts/content', 'search');
     endwhile;
-echo "</div>";
-echo "</section>";
-wp_reset_query();
-get_footer();
+    echo "</div>";
+    } 
+    
+
+    echo "</section>";
+    wp_reset_query();
+    get_footer();
 } else {
 get_header();
     echo "<section class='section'>";
@@ -65,10 +91,10 @@ get_header();
     echo "</section>";
 get_footer();
 ?>
-<script>
-    jQuery(document).ready(function($) {
-        jQuery('#wp-submit').addClass('button is-fullwidth is-link');
-    });
-</script>
-<?php
+        <script>
+        jQuery(document).ready(function($) {
+            jQuery('#wp-submit').addClass('button is-fullwidth is-link');
+        });
+        </script>
+        <?php
 }
