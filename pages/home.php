b7c4d9ad51216851f9bdf27abd6cb8e3d5da4b7d
Henry puttick <hputtick@me.com>
2019-06-30 21:23:55 +0700

    home page correct markup

--- pages/home.php
+++ pages/home.php
@@ -4,15 +4,16 @@
  * Template Name: Home page
  *
  */
-
 get_header();
 
+echo "<section class='section'>";
 while (have_posts()):
     the_post();
 
     the_content();
 
 endwhile; // End of the loop.
+echo "</section>";
 
 // get_sidebar();
 get_footer();
